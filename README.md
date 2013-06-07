280c5e37ac0ebd5988a3357623c4f96fa24c0ab3
Scott Block <scottrblock@gmail.com>
2013-06-07 02:56:31 -0400

    basic template done

---
+++ README.md
@@ -0,0 +1,12 @@
+Startup Shell New Site
+=
+
+**Fun Things:**
+
+- parrallax via skrollr.js
+- compass and scss to make life easier
+- full vertical screen craziness
+- an attempt at responsiveness
+
+
+Still need to clean up responsiveness, add more pages, and optimize assets.
--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-
+window.my_config = {
+  offset : parseInt($('#main-nav-list .first').css('margin-right'))
+} 
 
 var buildHomePage = function () {
   return {
@@ -11,25 +13,32 @@ var buildHomePage = function () {
          var $magicLine = $("#magic-line");
          var $curItem = $('.active', $mainNav);
 
-         var offset = parseInt($('#main-nav-list .first').css('margin-right'))
-
          $magicLine
              .width($curItem.width())
-             .css("left", $("a", $curItem).position().left - offset)
+             .css("left", $("a", $curItem).position().left - my_config.offset)
              .data("origLeft", $magicLine.position().left)
              .data("origWidth", $magicLine.width());
              
          $("#main-nav-list li a").click(function(e) {
-           $el = $(this);
-           leftPos = $el.position().left - offset;
-           newWidth = $el.parent().width();
-           $magicLine.stop().animate({
-               left: leftPos,
-               width: newWidth
-           }, 200);
+          $el = $(this);
+           buildHomePage.moveLine($el, $magicLine);
 
-           return false;
+          $.scrollTo( $el.attr('href'), 800, {offset:-80} );
+
+         return false;
          });
+    },
+
+    moveLine : function ($el, $magicLine){
+       leftPos = $el.position().left - my_config.offset;
+       newWidth = $el.parent().width();
+       $magicLine.stop().animate({
+           left: leftPos,
+           width: newWidth
+       }, 200);
+
+      $('#main-nav-list .active').removeClass('active');
+      $el.parent().addClass('active');
     },
 
     handleMediaQueries: function () {
@@ -61,6 +70,46 @@ var buildHomePage = function () {
 
     removeMagicLine: function () {
       $('#magic-line').remove();
+    },
+
+    repositionMagicLine: function (i) {
+        $('#main-nav-list .active').removeClass('active');
+        $('#main-nav-list li:eq(' + i + ')').addClass('active');
+        buildHomePage.reDrawMagicLine();
+    },
+
+    resizeBackgrounds: function () {
+      var h = 750;
+      var y = 1200;
+
+      var backZoom =$(window).height() /  (h * $(window).width() / 1200);
+      var percent =  (backZoom*100).toFixed(1) + "%"
+      $('.main-container-wrapper').css('height', ($(window).height() - 80)*3);
+      $('.main-container-wrapper').css('background-size', percent);
+      $('.section-content').css('height', $(window).height() -80);
+      $('.red-background').css('height', '100%');
+    },
+
+    listenForScrolls: function () {
+      $('.section-title').waypoint(function(d) {
+        var eq = $(this).attr('data-attr-count');
+        eq = parseInt(eq.charAt(eq.length - 1));  //kind of gross but better than upping scroll throttle
+
+        if(d == 'down'){
+          eq = eq + 1
+        }
+
+        if(eq >= 0){
+          buildHomePage.moveLine($('#main-nav-list li:eq(' + eq + ') a'), $('#magic-line'));
+        }
+      });
+    },
+
+    listenForButtonClicks: function () {
+      $('.bottom-button').click(function(){
+        $.scrollTo( $(this).attr('href'), 800, {offset:-80} );
+        return false;
+      });
     }
 
   }
@@ -69,15 +118,33 @@ var buildHomePage = function () {
 }();
 
 
-
-
-
-
-
-
-
-
 $(document).ready(function () {
+  var s = skrollr.init({
+    forceHeight: false
+  });
+
   buildHomePage.buildMagicLine();
   buildHomePage.handleMediaQueries();
+  buildHomePage.resizeBackgrounds();
+  buildHomePage.listenForScrolls();
+  buildHomePage.listenForButtonClicks();
+
+  skrollr.menu.init(s, {
+      animate: true, //skrollr will smoothly animate to the new position using `animateTo`.
+      duration: 600, //How long the animation should take in ms.
+      easing: 'sqrt' //The easing function to use.
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
